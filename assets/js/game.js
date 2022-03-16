9aa8344de4ac7dac3e3f4519e21494a4862ece91
vincent teune <vincentteune@gmail.com>
2022-03-16 11:23:30 -0400

    3.16.22
    its getting closer to working,

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,24 @@
+
+//timeDisplayEl is the actual countdown
+var timeDisplayEl = document.getElementById('time');
+//timeEl is the header element that contains the countdown
 var timerEl = document.getElementById('timer');
+//scoreDisplayEl is where the score cariable is diaplyed
+var scoreDisplayEl = document.getElementById('score');
+//scoreEl is the header that contains the score display
+var scoreEl = document.getElementById('scoreTracker')
 var startEl = document.querySelector('#start');
 var option1El = document.querySelector('#option1');
 var option2El = document.querySelector('#option2');
 var option3El = document.querySelector('#option3');
 var option4El = document.querySelector('#option4');
 var questionEl = document.querySelector('#question');
-var answers = document.querySelector(".answers")
-var options = document.querySelector(".options")
-var button = document.getElementsByTagName("button")
-var data = button.getAttribute('data');
-// let options = document.querySelector([])
-var timeLeft = 40
-var score = 0;
-questionsObj = 0;
+var answers = document.querySelector(".answers");
+var options = document.querySelector(".options");
+var button = document.getElementsByTagName("button");
+let timeLeft = 40;
+let score = 0;
+var questionsObj = 0;
 const questions = [
 
   {
@@ -25,7 +31,6 @@ const questions = [
     data3: 'incorrect',
     answer4: "answer4 goes here",
     data4: 'incorrect',
-    // correctAnswer: option1El,
   }, 
   {
     question: "question2 goes here",
@@ -50,54 +55,70 @@ const questions = [
   }
 ];
 
+function hideQuestions() {
+  option1El.setAttribute('style', 'display: none;');
+  option2El.setAttribute('style', 'display: none;');
+  option3El.setAttribute('style', 'display: none;');
+  option4El.setAttribute('style', 'display: none;');
+  scoreEl.setAttribute('style', 'display: none;');
+  timerEl.setAttribute('style', 'display: none;');
+}
+
+function startQuiz(event) {
+  startEl.setAttribute('style', 'display: none;');
+  option1El.setAttribute('style', 'display: visible;');
+  option2El.setAttribute('style', 'display: visible;');
+  option3El.setAttribute('style', 'display: visible;');
+  option4El.setAttribute('style', 'display: visible;');
+  scoreEl.setAttribute('style', 'display: visible;');
+  timerEl.setAttribute('style', 'display: visible;');
+
+  timeDisplayEl.textContent = timeLeft;
+  scoreDisplayEl.textContent = score;
+
+  generateQuestions();
+  countdown();
+}
 
 function countdown() {
 
   var timeInterval = setInterval(function () {
+    
     if (timeLeft > 0) {
       timeLeft--;
-      timerEl.textContent = timeLeft;
     } else {
-      timerEl.textContent = (' ');
+      timeDisplayEl.textContent = (' ');
       clearInterval(timeInterval);
       displayMessage();
-      option1El.setAttribute('style', 'display: none;')
-      option2El.setAttribute('style', 'display: none;')
-      option3El.setAttribute('style', 'display: none;')
-      option4El.setAttribute('style', 'display: none;')
+      option1El.setAttribute('style', 'display: none;');
+      option2El.setAttribute('style', 'display: none;');
+      option3El.setAttribute('style', 'display: none;');
+      option4El.setAttribute('style', 'display: none;');
+      
 
     }
   }, 1000);
 }
- 
 
-startEl.addEventListener('click', function () {
-  startEl.setAttribute('style', 'display: none;');
-  displayQuestions();
-  countdown();
-  generateQuestions();
-  
-})
-
-function hideQuestions() {
-  option1El.setAttribute('style', 'display: none;')
-  option2El.setAttribute('style', 'display: none;')
-  option3El.setAttribute('style', 'display: none;')
-  option4El.setAttribute('style', 'display: none;')
-}
-
-function displayQuestions() {
-  option1El.setAttribute('style', 'display: visible;')
-  option2El.setAttribute('style', 'display: visible;')
-  option3El.setAttribute('style', 'display: visible;')
-  option4El.setAttribute('style', 'display: visible;')
-}
-
-function checkAnswer(){
-  if (dataId = correct ) {
-    var check = true;
+function scoreTracker() {
+  if (score >= 0) {
+    // scoreEl.textContent = (`Your Score: ${score}`);
   } else {
-    check = false;
+    scoreEl.textContent = (' ')
+  }
+}
+
+function verifyAnswerHandler(event){
+  var targetEl = event.target;
+  var targetData = targetEl.getAttribute("class");
+
+
+  if (targetData === 'correct' ) {
+    score = score + 10;
+
+
+  } else if (targetData === 'incorrect') {
+    timeLeft = timeLeft - 10;
   }
 }
 
@@ -116,122 +137,23 @@ function generateQuestions() {
     questionEl.textContent = questions[questionsObj].question;
     
     option1El.textContent = questions[questionsObj].answer1;
-    option1El.setAttribute('data', questions[questionsObj].data1)
-    let dataId = this.data;
+    option1El.setAttribute('class', questions[questionsObj].data1);
 
     option2El.textContent = questions[questionsObj].answer2;
-    option2El.setAttribute('data', questions[questionsObj].data2)
-    // option1El.on('click', checkAnswer)
+    option2El.setAttribute('class', questions[questionsObj].data2);
 
     option3El.textContent = questions[questionsObj].answer3;
-    // option1El.on('click', checkAnswer)
+    option3El.setAttribute('class', questions[questionsObj].data3);
 
     option4El.textContent = questions[questionsObj].answer4;
-    // option1El.on('click', checkAnswer)
-    console.log(option1El)
-    console.log(option2El)
+    option4El.setAttribute('class', questions[questionsObj].data4);
+
 }
-// look at task handler, maybe use event.target.addEventListener?
-//also look at taskStatusChangeHandler for an idea on how to make this work.
-
-    options.addEventListener("click", function checkAnswer() {
-      if (check) {
-        score = score + 10;
-        questionsObj++;
-      } else { 
-        score = score - 10;
-        timeLeft = timeLeft - 10;
-        questionsObj++;
-      }
-    })
-    //   console.log(score)
-    //   })
-
-    // var correctAnswer = questions[0].solution;
-
-    // correctAnswer.addEventListener("click", solution)
-
-    // var inCorrectAnswer = questions[0].solution;
-
-    // inCorrectAnswer.addEventListener("click", solution);
 
 
 
-// var correctAnswer = questions[0].solution;
+startEl.addEventListener('click', startQuiz);
 
-//     correctAnswer.addEventListener("click", solution);
-
-
-
-// function checkAnswer () {
-//   if 
-// }
-// function solution(){
-//   score = score + 10;
-//   questionsObj = questionsObj + 1;
-// }
-
-
-
-// function question1() {
-//   questionEl.textContent = ('Question 1');
-//   option1El.textContent = ('answer option 1');
-//   option2El.textContent = ('answer option 2');
-//   option3El.textContent = ('answer option 3');
-//   option4El.textContent = ('answer option 4');
-
-
-//   const button = document.querySelectorAll("button");
-
-//     button.addEventListener("click", function(){
-//       console.log("This works!")
-//       console.log(this)
-//     })
-
-
-// option1El.addEventListener('click', function() {
-//   score = score + 1;
-//   console.log(this);
-
-// });
-
-// if (option1El) {
-//   score = score + 1;
-// } else {
-//   score = score - 10;
-// }
-
-
-
-// function question2() {
-//   questionEl.textContent = ('Question 1');
-//   option1El.textContent = ('answer option 1');
-//   option2El.textContent = ('answer option 2');
-//   option3El.textContent = ('answer option 3');
-//   option4El.textContent = ('answer option 4');
-
-//   if (document.querySelector('#option1').clicked) {
-//     option1El = true;
-//   }
-
-//   if (document.querySelector('#option2').clicked) {
-//     option2El = true;
-//   }
-
-//   if (document.querySelector('#option3').clicked) {
-//     option3El = true;
-//   }
-
-//   if (document.querySelector('#option4').clicked) {
-//     option4El = true;
-//   }
-
-//   if (option1El) {
-//     score = score + 1;
-//   } else if (option2El || option3El || option4El) {
-//     score = score - 10;
-//   }
-
-// }
+options.addEventListener('click', verifyAnswerHandler);
 
 hideQuestions();
