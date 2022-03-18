3be5d23ac85306c013e235c0956abed9c2c64453
cobalt88 <vincentteune@gmail.com>
2022-03-18 19:57:34 -0400

    3.18.22
    now it only works for one run though again. on subsequent run througs the time does not initilize

--- .history/assets/js/game_20220318195331.js
+++ .history/assets/js/game_20220318195331.js
@@ -301,6 +301,10 @@ function saveScore(event) {
 
   localStorage.setItem('playerData', JSON.stringify(scoreData))
   
+  startEl.setAttribute('style', 'display: visible;');
+  hideElements();
+  questionEl.setAttribute('style', 'display: none;');
+  
 } 
 
 
