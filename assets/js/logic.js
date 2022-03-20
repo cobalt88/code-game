533c830b11d7acccbb38a29b28f99f33a61ec8ae
vincent teune <vincentteune@gmail.com>
2022-03-20 10:12:37 -0400

    3.20.22
    Added in comments in logic.js for main landing page.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,15 +4,18 @@ const thirdEl = document.querySelector('#third');
 const fourthEl = document.querySelector('#fourth');
 const fifthEl = document.querySelector('#fifth');
 
+//load the stringified array from local storage and parse it back into an unstringified array.
 var storedScores = JSON.parse(localStorage.getItem('playerData')); 
 if (storedScores === null) {
   storedScores = [];
 }
 
+//  sort the storedScores array in decending order according the value of the score property,
 storedScores.sort((a,b) => {
     return b.score - a.score;
   })
 
+  // leaderboard
 firstEl.textContent = (`Player: ${storedScores[0].player}, Score: ${storedScores[0].score} points`)
 secondEl.textContent = (`Player: ${storedScores[1].player}, Score: ${storedScores[1].score} points`)
 thirdEl.textContent = (`Player: ${storedScores[2].player}, Score: ${storedScores[2].score} points`)
